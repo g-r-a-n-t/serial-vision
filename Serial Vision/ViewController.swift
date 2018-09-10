1e096778bfd4ee786fb1249abb3344436615dd7e
Brandon Roehl <brandon.roehl@jamf.com>
2018-09-10 16:28:03 -0500

    Updates to get the image to load

--- Serial Vision/ViewController.swift
+++ Serial Vision/ViewController.swift
@@ -8,13 +8,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    // MARK: - Storyboard References
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    // Question so you must test for existance
+    var image: UIImage? {
+        didSet {
+            self.imageView.image = image
+        }
+    }
+    
+    var imagePicker: UIImagePickerController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.resultLabel.text = "Select an image to begin"
+        
+        self.imagePicker = UIImagePickerController()
+        self.imagePicker.delegate = self
     }
 
-
+    // MARK: - IBActions
+    
+    @IBAction func selectImageClicked(_ sender: Any) {
+        print("Image Clicked")
+        present(self.imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func runAction(_ sender: Any) {
+        print("Run the algorithum to get the serial number")
+        print(self.image ?? "No image selected")
+        // TODO: - Implement the image reading
+    }
+    
+    internal func imagePickerController(_ picker: UIImagePickerController,
+                                        didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        // Gracefully fail
+        self.image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        
+        if self.image == nil {
+            print("This info marker failed to confrom")
+        }
+        
+        // Dismiss the picker to return to original view controller.
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
 

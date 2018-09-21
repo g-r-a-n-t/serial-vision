0c7030d0045963c82951227e94c4e1441f05ea42
Brandon Roehl <brandon.roehl@jamf.com>
2018-09-20 20:08:21 -0500

    Starting the history view for storing past scans and navigating down with

--- Serial Vision/Controllers/HistoryTableController.swift
+++ Serial Vision/Controllers/HistoryTableController.swift
@@ -17,30 +17,37 @@ class HistoryTableController: UITableViewController {
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+         self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return CoreHistory.getCount(from: nil)
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "serialCell", for: indexPath) as! HistoryCell
 
-        // Configure the cell...
+        let history = CoreHistory.get(from: nil)[indexPath.item]
+        let computer = CoreComputer.get(serial: history.serialNumber)
+        
+        cell.deviceName.text = computer?.deviceName
+        cell.serialNumber.text = history.serialNumber
+        
+//        cell.textLabel?.text = history.code
+//        if let date = history.date as Date? {
+//            cell.detailTextLabel?.text = DateFormatter.localizedString(from: date, dateStyle: .none, timeStyle: .short)
+//        }
 
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
@@ -50,41 +57,30 @@ class HistoryTableController: UITableViewController {
     }
     */
 
-    /*
     // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
+            // Delete history item
+            CoreHistory.get(from: nil)[indexPath.item].delete()
+            
             // Delete the row from the data source
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
 
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        if let indexPath = self.tableView.indexPathForSelectedRow {
+            let history = CoreHistory.get(from: nil)[indexPath.item]
+            
+            if let controller = segue.destination as? ComputerController {
+                controller.serialNumber = history.serialNumber
+            }
+        }
     }
-    */
 
 }
